7f71b175bc31194c45e37ba81576a823597d1b67
Gustavo Cereza <113127758+GustavoCereza@users.noreply.github.com>
2023-04-10 20:17:32 -0300

    Update installer.sh

--- installer.sh
+++ installer.sh
@@ -1,5 +1,5 @@
-sudo apt update -y
-sudo apt-get update -y
+sudo apt update ;
+sudo apt-get update ;
 
 git clone https://github.com/kersing/lora_gateway.git
 git clone https://github.com/kersing/paho.mqtt.embedded-c.git
@@ -8,47 +8,47 @@ git clone https://github.com/kersing/protobuf-c.git
 git clone https://github.com/kersing/packet_forwarder.git
 git clone https://github.com/google/protobuf.git
 
-sudo apt install protobuf-compiler
-sudo apt install libprotobuf-dev
-sudo apt install libprotoc-dev
+sudo apt -y install protobuf-compiler ;
+sudo apt -y install libprotobuf-dev ;
+sudo apt -y install libprotoc-dev ;
 
-sudo apt-get install automake
-sudo apt install libtool
-sudo apt install autoconf
+sudo apt-get -y install automake ;
+sudo apt -y install libtool ;
+sudo apt -y install autoconf ;
 
-sudo apt install libftdi1
-sudo apt install libftdi-dev
-sudo apt install swig
-sudo apt install python-dev
-sudo apt search libusb
-sudo apt install libusb-1.0-0
-sudo apt install libusb-1.0-0-dev
+sudo apt -y install libftdi1 ;
+sudo apt -y install libftdi-dev ;
+sudo apt -y install swig ;
+sudo apt -y install python-dev ;
+sudo apt -y search libusb ;
+sudo apt -y install libusb-1.0-0 ;
+sudo apt -y install libusb-1.0-0-dev ;
 
-cd packet_forwarder/
+cd packet_forwarder
 cd mp_pkt_fwd/
-sudo rm -rf build-pi.sh
-sudo cp /home/pi/pkt_fwd_raspbian/build-pi.sh ./
-sudo chmod +x ./build-pi.sh
-sudo ./build-pi.sh
+rm -rf build-pi.sh
+cp /home/pi/pkt_fwd_raspbian/build-pi.sh ./
+chmod +x ./build-pi.sh
+sudo ./build-pi.sh ;
 
 cd /opt/elcereza
-sudo cp /home/pi/pkt_fwd_raspbian/global_conf.json ./
-sudo cp /home/pi/pkt_fwd_raspbian/start.sh ./
-sudo chmod +x start.sh
+cp /home/pi/pkt_fwd_raspbian/global_conf.json ./
+cp /home/pi/pkt_fwd_raspbian/start.sh ./
+chmod +x start.sh
 
 cd /lib/systemd/system
-sudo cp /home/pi/pkt_fwd_raspbian/elcereza.service ./
-sudo chmod +x elcereza.service
+cp /home/pi/pkt_fwd_raspbian/elcereza.service ./ ;
+chmod +x elcereza.service
 
-sudo systemctl daemon-reload
-sudo systemctl enable elcereza.service
-sudo systemctl start elcereza.service
-sudo systemctl status elcereza.service
+sudo systemctl daemon-reload ;
+sudo systemctl enable elcereza.service ;
+sudo systemctl start elcereza.service ;
+sudo systemctl status elcereza.service ;
 
-cd 
-sudo rm -rf lora_gateway
-sudo rm -rf packet_forwarder
-sudo rm -rf paho.mqtt.embedded-c
-sudo rm -rf protobuf
-sudo rm -rf protobuf-c
-sudo rm -rf ttn-gateway-connector
+cd
+rm -rf lora_gateway
+rm -rf packet_forwarder
+rm -rf paho.mqtt.embedded-c
+rm -rf protobuf
+rm -rf protobuf-c
+rm -rf ttn-gateway-connector
